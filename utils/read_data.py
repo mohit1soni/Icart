e876f29f164a44def818a11e0ef5c5ab330aa4c2
Mohit_Soni <mohitsoni2091996@gmail.com>
2019-01-10 15:51:08 +0530

    kinnect depth image has been taken and observed in gray scale

---
+++ utils/read_data.py
@@ -0,0 +1,10 @@
+import pickle
+import numpy
+
+class read_data(object):
+    def __init__(self,path,dataset):
+        self.path=path
+        self.dataset=dataset
+
+    def read_traffic_sign_data(self):
+        pass
