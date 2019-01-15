d181972d9d6e639417ce9aa48a23bc2e14953d85
Mohit_Soni <mohitsoni2091996@gmail.com>
2019-01-15 20:17:30 +0530

    kineect making problem

--- utils/read_data.py
+++ utils/read_data.py
@@ -2,9 +2,25 @@ import pickle
 import numpy
 
 class read_data(object):
-    def __init__(self,path,dataset):
-        self.path=path
-        self.dataset=dataset
+    def __init__(self,path,dataset,val=False):
+        if val:
+            self.train_path=path[0]
+            self.val_path=path[1]
+            self.test_path=path[2]
+        else:
+            self.train_path=path[0]
+            self.test_path=path[1]
+        self.dataset_name=dataset
 
     def read_traffic_sign_data(self):
-        pass
+        with open(self.train_path,mode='rb') as f:
+            train=pickle.load(f)
+        with open(self.test_path,mode='rb') as f:
+            test=pickle.load(f)
+        return train,test
+
+    def split_traffic_sign_data(self):
+        train,test=self.read_traffic_sign_data()
+        x_train,y_train=train['features'],train['labels']
+        x_test,y_test=test['features'],test['labels']
+
