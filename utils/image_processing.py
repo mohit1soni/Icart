9bdf08f492795c7b7fc403ae6587553bb4a1e5e0
Mohit_Soni <mohitsoni2091996@gmail.com>
2019-01-17 23:05:19 +0530

    video_file_for car detection generated

--- utils/image_processing.py
+++ utils/image_processing.py
@@ -1,6 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+import os
 import cv2
 # import matplotlib.image as imgplt
 class image_processing(object):
@@ -13,7 +13,7 @@ class image_processing(object):
     def __init__(self):
         self.left_fit=list()
         self.right_fit=list()
-    
+
     def img_show(self,image,title='image',Cmap='',fig_num=1,group_view=False,save=False):
         plt.figure(fig_num)
         plt.title(title)
@@ -26,7 +26,7 @@ class image_processing(object):
             plt.savefig("../Results/image_processing/Track/"+title+".png")
         if group_view is True:
             plt.show()
-    
+
     def video_capture(self,video_path,show=False,window_name="video_frame",save=False):
         cap=cv2.VideoCapture(video_path)
         if save:
@@ -84,7 +84,7 @@ class image_processing(object):
     def region_of_interest(self):
         self.image_height=self.canny_image.shape[0]
         polygon= np.array([(200,self.image_height),(1100,self.image_height),(550,250)])
-        #This polygon drawing has to be adaptive so that it will always detect lines           
+        #This polygon drawing has to be adaptive so that it will always detect lines
         self.mask=np.zeros_like(self.canny_image)
         cv2.fillPoly(self.mask,np.int32([polygon]),255)
         self.masked_image=cv2.bitwise_and(self.canny_image,self.mask)
@@ -94,7 +94,7 @@ class image_processing(object):
         self.line_image=np.zeros_like(self.image)
         left_fit=list()
         right_fit=list()
-        
+
         if lines is not None:
             for line in lines:
                 x1,y1,x2,y2=line.reshape(4)
@@ -105,37 +105,37 @@ class image_processing(object):
                     left_fit.append((slope,intercept))
                 else:
                     right_fit.append((slope,intercept))
-               
-        
+
+
         if len(left_fit)==0:
             left_fit=self.left_fit
         else:
             self.left_fit=left_fit
-            
+
         left_fit_average=np.average(left_fit,axis=0)
         right_fit_average=np.average(right_fit,axis=0)
-        
+
         y1=self.image.shape[0]
         y2=int(y1*3/5)
-        
+
         print(left_fit_average)
-    
+
         left_slope=left_fit_average[0]
         left_intercept=left_fit_average[1]
 
         right_slope=right_fit_average[0]
-        right_intercept=right_fit_average[1]        
-        
-        
-        x1_left=int((y1-left_intercept)/left_slope)        
+        right_intercept=right_fit_average[1]
+
+
+        x1_left=int((y1-left_intercept)/left_slope)
         x2_left=int((y2-left_intercept)/left_slope)
-        
-        x1_right=int((y1-right_intercept)/right_slope)        
+
+        x1_right=int((y1-right_intercept)/right_slope)
         x2_right=int((y2-right_intercept)/right_slope)
-        
+
         cv2.line(self.line_image,(x1_left,y1),(x2_left,y2),(255,0,0),10)
         cv2.line(self.line_image,(x1_right,y1),(x2_right,y2),(255,0,0),10)
-        
+
         self.track_image=cv2.addWeighted(self.line_image,0.6,self.image,0.4,0.4)
         if show:
             self.img_show(self.track_image,title=window_name,Cmap='',fig_num=6,group_view=False,save=False)
@@ -148,13 +148,25 @@ class image_processing(object):
             cv2.waitKey(1)
 #            self.img_show(self.masked_image,title=window_name,Cmap='gray',fig_num=5,group_view=False)
         self.hough_transform()
+    def make_video_from_images(self,images_path,video_file_name):
+        images = [img for img in os.listdir(images_path) if img.endswith('.png')]
+        frame=cv2.imread(os.path.join(images_path,images[0]))
+        height,width,layers = frame.shape
+        video=cv2.VideoWriter(video_file_name,0,5,(width,height))
+        for image in images:
+            video.write(cv2.imread(os.path.join(images_path,image)))
+        cv2.destroyAllWindows()
+        video.release()
 
 
 def main():
-    path='../data/test_images/solidWhiteCurve.jpg'
-    video_path='../data/test_videos/test2.mp4'
+    # path='../data/test_images/solidWhiteCurve.jpg'
+    # video_path='../data/test_videos/test2.mp4'
+    images_path='../../Results/lane'
     pro=image_processing()
-    pro.video_capture(video_path,show=True,save=True)
+    video_name='lane_object_detect.avi'
+    pro.make_video_from_images(images_path,video_name)
+    # pro.video_capture(video_path,show=True,save=True)
 #    image=pro.single_img_read(path,show=True)
 #    pro.canny_edge_detect()
 #    pro.find_lanes()
