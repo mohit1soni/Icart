7a42a3ad67487412e31bb72c3bfce684b3e8a433
Mohit_Soni <mohitsoni2091996@gmail.com>
2019-02-21 23:45:09 +0530

    Neural Net Added

--- control/steering/data_read.py
+++ control/steering/data_read.py
@@ -4,34 +4,45 @@ import cv2
 import csv
 from decimal import *
 import matplotlib.pyplot as plt
+import pandas as pd
 
 data_path="../../../data/driving_dataset"
 batch_size = 100
-labels_file=open("../../../data/driving_dataset/data.txt",'r')
+labels_file=pd.read_csv("../../../data/driving_dataset/data.txt",delimiter=' ')
 
 def assign_bin(decimal_value,min_steer,max_steer):
-    int_range=decimal_value.quantize(Decimal('001.'))
+    int_range=decimal_value
     if int(np.divide(int_range,10)) == 0:
-        return int(int_range)
+        return np.round(int_range)
     else:
-        new_int_range=int(np.divide(int_range,10))
+        new_int_range=np.divide(int_range,10)
         # print(new_int_range)
-        return new_int_range
-min_steer=-159.931
-max_steer=501.784
+        return np.round(new_int_range)
+
+labels=np.asarray(labels_file['steer'],dtype=np.float32)
+min_steer=min(labels)
+max_steer=max(labels)
+print(type(labels[0]))
+
+new_label=list()
+for i in range(labels.shape[0]):
+    label=assign_bin(labels[i],min_steer,max_steer)
+    new_label.append(label)
 
 
-labels =list()
-for row in labels_file:
-    k=row.split(" ")
-    k[0]=data_path+"/"+k[0]
-    labels.append(k[0])
-    int_range = assign_bin(Decimal(k[1]),min_steer,max_steer)
-    labels.append(int_range)
+# labels =list()
+# for row in labels_file:
+#     k=row.split(" ")
+#     k[0]=data_path+"/"+k[0]
+#     labels.append(k[0])
+#     int_range = assign_bin(Decimal(k[1]),min_steer,max_steer)
+#     labels.append(int_range)
 
-labels=np.reshape(labels,(int(len(labels)/2),2))
-img_samp=cv2.imread(labels[0,0])
-(img_height,img_width,n_channels)=img_samp.shape
+
+# labels=np.reshape(labels,(int(len(labels)/2),2))
+
+# img_samp=cv2.imread(labels[0,0])
+# (img_height,img_width,n_channels)=img_samp.shape
 
 
 n_batches=int(labels.shape[0]/batch_size)
@@ -39,34 +50,34 @@ n_batches=int(labels.shape[0]/batch_size)
 batch_save_path="../../../data/data_drive/"+"/batches/"
 label_save_path="../../../data/data_drive/"+"/labels/"
 
-#  For the creation of batches
-max_steer=max(labels[0:labels.shape[0],1])
-min_steer=min(labels[0:labels.shape[0],1])
+# #  For the creation of batches
+max_steer=max(new_label)
+min_steer=min(new_label)
 
-min_index=np.argmin(labels[:,1])
-max_index=np.argmax(labels[:,1])
+min_index=np.argmin(new_label)
+max_index=np.argmax(new_label)
 
-# Just For Checking Purposes
+# # Just For Checking Purposes
 print(min_steer,max_steer)
 print(min_index,max_index)
-print(labels[min_index-1,1],labels[max_index-1,1])
-
+# print(labels[min_index,1],labels[max_index,1])
+# print(type(labels[100,1]))
 ##  For the visualization of the data
 
-# shorted_list=sorted(labels[:,1],key=int)
-# plt.hist(shorted_list,bins=30,histtype="step")
-# plt.xlabel("Steering Angle")
-# plt.ylabel("No of Occurance")
-# plt.title("Data Visualization with Non-linear Binning Function")
-# plt.savefig("../../Results/Bining_histogram_steering_Ang.png")
-# plt.show()
+shorted_list=sorted(new_label,key=int)
+plt.hist(shorted_list,bins=30,histtype="step")
+plt.xlabel("Steering Angle")
+plt.ylabel("No of Occurance")
+plt.title("Data Visualization with Non-linear Binning Function")
+plt.savefig("../../Results/Bining_histogram_steering_Ang.png")
+plt.show()
 
 ##  For the generation of data and labels
 
 # for i in range(n_batches):
 #     # batch=np.zeros((batch_size,img_height,img_width,n_channels))
 #     steering=np.zeros((batch_size),dtype=float)
-#     steering=labels[i*batch_size:(i+1)*batch_size,1]
+#     steering=new_label[i*batch_size:(i+1)*batch_size]
 #     # filename=labels[i*batch_size:(i+1)*batch_size,0]
 #     # for j in range(batch_size):
 #     #     batch[j,:,:,:]=cv2.imread(filename[j])
@@ -79,7 +90,7 @@ print(labels[min_index-1,1],labels[max_index-1,1])
 ##  For the testing of the generated batches and labels
 
 # batch1=np.load(batch_save_path+"batch_0.npy")
-# label1=np.load(label_save_path+"label_0.npy")
+# label1=np.load(label_save_path+"label_15.npy")
 # print(label1[10])
 
 
--- control/steering/model.py
+++ control/steering/model.py
@@ -29,43 +29,176 @@ root_dir=os.getcwd()
 restore_from=os.path.join(root_dir,'saved_model','model_name')
 log_dir=os.path.join(root_dir,'log')
 
-# Variable Initialization
+## Variable Initialization
+
+# Image Vriables
 height=256
 width=455
 batch_size=100
-nb_epoch=10
-saved_model_step=10
+num_classes=16+1+50
+
+# Network Variables
+epochs=200
 max_to_keep=4
+regularizer_scale=0.2
+display_step=1
+batch_display_step=50
+save_model_step=10
+num_train_batch=len(x_train)
+num_val_batch=len(x_val)
+learning_rate=0.0001
+
 
 # Defining Time_based folder creation for logs
 now=datetime.now()
 time_stamp=now.strftime("%Y_%m_%d_%H_%M_%S")
 log_dir=os.path.join(log_dir,time_stamp)
-# os.makedirs(log_dir)
+os.makedirs(log_dir)
 saved_model_dir=os.path.join(root_dir,'saved_model',time_stamp)
-# os.makedirs(saved_model_dir)
+os.makedirs(saved_model_dir)
 print(saved_model_dir)
 
 
 import tensorflow as tf
 
-steering=tf.graph()
+steering=tf.Graph()
 print("Genration of Tensorflow Graph")
 
 with steering.as_default():
-    global_step=tf.variable(0)
+    global_step=tf.Variable(0)
     with tf.name_scope('Inputs'):
         input_data=tf.placeholder(tf.float32,shape=(None,height,width,3),name="images_in")
         input_labels=tf.placeholder(tf.float32,shape=(None),name="steeting_angle")
         is_training=tf.placeholder_with_default(False,shape=())
         condition=tf.cast(is_training,tf.float32)
         pred=tf.less(condition,0.5)
-        one_hot=tf.one_hot(input_labels,)
+        one_hot=tf.one_hot(input_labels,num_classes,dtype=tf.float32,name="One_hot_labels")
+        input_tensor=tf.subtract(tf.multiply(2.0,tf.divide(input_data,255.0)),1.0)
+
     with tf.name_scope("Driver_Net"):
+        regularizer=tf.contrib.layers.l2_regularizer(scale=regularizer_scale)
+        net=tf.layers.conv2d(input_tensor,filters=3,karnel_size=5,strides=(2,2),padding='valid',activation=tf.nn.elu,name="layer0",kernel_regularizer=regularizer)
+        net=tf.layers.dropout(net,rate=0.2)
 
+        net_mean,net_variance=tf.nn.moments(net,[0])
+        net = tf.nn.batch_normalization(net,mean=net_mean,variance=net_variances,offset=None,scale=None,variance_epsilon=0.001)
 
+        net=tf.layers.conv2d(net,filters=24,karnel_size=5,strides=(2,2),padding='valid',activation=tf.nn.elu,name="layer1",kernel_regularizer=regularizer)
+        net=tf.layers.dropout(net,rate=0.2)
 
+        net_mean,net_variance=tf.nn.moments(net,[0])
+        net = tf.nn.batch_normalization(net,mean=net_mean,variance=net_variances,offset=None,scale=None,variance_epsilon=0.001)
 
+        net=tf.layers.conv2d(net,filters=36,karnel_size=5,strides=(2,2),padding='valid',activation=tf.nn.elu,name="layer2",kernel_regularizer=regularizer)
+        net=tf.layers.dropout(net,rate=0.2)
+
+        net_mean,net_variance=tf.nn.moments(net,[0])
+        net = tf.nn.batch_normalization(net,mean=net_mean,variance=net_variances,offset=None,scale=None,variance_epsilon=0.001)
+
+        net=tf.layers.conv2d(net,filters=48,karnel_size=5,strides=(2,2),padding='valid',activation=tf.nn.elu,name="layer3",kernel_regularizer=regularizer)
+        net=tf.layers.dropout(net,rate=0.2)
+
+        net=tf.layers.conv2d(net,filters=64,karnel_size=3,strides=(1,1),padding='valid',activation=tf.nn.elu,name="layer4")
+        net=tf.layers.dropout(net,rate=0.2)
+
+        net=tf.layers.conv2d(net,filters=64,karnel_size=3,strides=(1,1),padding='valid',activation=tf.nn.elu,name="layer5")
+        net=tf.layers.dropout(net,rate=0.2)
+
+        net=tf.contrib.layers.flatten(net)
+
+        net=tf.layers.dense(net,100,activation=tf.nn.elu,name='Dense_1')
+        net=tf.layers.dropout(net,rate=0.5,is_training=is_training)
+
+        net=tf.layers.dense(net,50,activation=tf.nn.elu,name='Dense_2')
+        net=tf.layers.dropout(net,rate=0.5,is_training=is_training)
+
+        net=tf.layers.dense(net,10,activation=tf.nn.elu,name='Dense_3')
+        net=tf.layers.dropout(net,rate=0.5,is_training=is_training)
+
+        output=tf.layers.dense(net,1,name="output_layer")
+
+    with tf.name_scope("Training"):
+        loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=output,labels=one_hot),name="loss")
+        optimize=tf.train.AdadeltaOptimizer(learning_rate=learning_rate,name="optimizer").minimize(loss,global_step=global_step)
+
+        #Summery Log
+        training_loss_summery=tf.summary.scalar("Training_Loss",loss)
+        val_loss_summary=tf.summary.scalar("Val_loss",loss)
+
+    with tf.name_scope("Accuracy"):
+        pred_prob=tf.nn.softmax(output)
+        pred_labels=tf.argmax(pred_prob,1,name="predicted_labels")
+        train_acc=tf.metrics.accuracy(input_labels,pred_labels,name="Accuracy")[1]
+        val_acc=tf.metrics.accuracy(input_labels,pred_labels,name="Accuracy")[1]
+
+        # Summery and Logs
+        train_acc_summary=tf.summary.scalar('Training_arruracy',train_acc*100)
+        val_acc_summary=tf.summary.scalar('Validation_arruracy',val_acc*100)
+
+        train_summary_merged=tf.summary.merge([train_acc_summary,training_loss_summery])
+        val_summary_merged=tf.summary.merge([val_acc_summary,val_loss_summery])
+
+    saver=tf.train.Saver(max_to_keep=max_to_keep)
+
+tf.reset_default_graph()
+
+print("Started Running Session .. ")
+with tf.Session(graph=steering) as sess:
+    sess.run(tf.global_variables_initializer())
+    sess.run(tf.local_variables_initializer())
+    #  For Restoring The previous model
+    if tf.train.latest_checkpoint(restore_from) is not None:
+        print("Restoring Model")
+        saver.restore(sess,tf.train.latest_checkpoint(restore_from))
+    # Defining Regular writer discriptions
+    writer=tf.summary.FileWriter(log_dir)
+    writer.add_graph(sess.graph)
+    writer.flush()
+
+    for epoch in range(epochs):
+        epoch_start_time=time.time()
+        # Training Start here
+        for batch_index in range(num_train_batch):
+            batch_start_time=time.time()
+            data=np.load(x_train[batch_index])
+            label=np.load(y_train[batch_index])
+            feed_dict={input_data:data,input_labels:label,is_training:True}
+            sess.run(optimize,feed_dict=feed_dict)
+            # For the Display Purpose
+            if batch_index%(batch_display_step) == 0 and epoch%(display_step)==0:
+                print('Epoch: %d <===>'%(epoch),
+                      '%.2f Percent Completed'%((batch_idx*100.0/num_train_batches)),
+                      '[Batch Size--> %d Images <===> Batch Process Time--> %.2f Seconds]'%(data.shape[0],time.time()-batch_start_time))
+
+        if epoch%display_step == 0:
+            # Calculating Validation Accuracy
+            random_index=np.random.randint(0,x_val.shape[0])
+            sample_val_data=np.load(x_val[random_index])
+            sample_val_label=np.load(y_val[random_index])
+            feed_dict={input_data:sample_val_data,input_labels:sample_val_label,is_training:False}
+            val_accuracy,val_summary=sess.run([val_acc,val_summary_merged],feed_dict)
+
+            # Calculating Training Accuracy
+            random_index=np.random.randint(0,x_val.shape[0])
+            sample_train_data=np.load(x_train[random_index])
+            sample_train_label=np.load(y_train[random_index])
+            feed_dict={input_data:sample_train_data,input_labels:sample_train_label,is_training:False}
+            loss_op,train_accuracy,train_summary=sess.run([loss,train_acc,train_summary_merged],feed_dict)
+
+            # Printing the Output
+            print('Epoch: %d <===>'%(epoch),'(val accuracy-->%.2f) (training loss-->%.2f) (training accuracy-->%.2f)'%(val_accuracy,loss_op,train_accuracy))
+            print('...Writing Logs in %s...'%(time_stamp))
+            writer.add_summary(train_summary,epoch)
+            writer.flush()
+            writer.add_summary(val_summary,epoch)
+            writer.flush()
+            #writer.add_summary(temp_op,epoch)
+            print('[Epoch Process Time--> %.2f Seconds]'%(time.time()-epoch_start_time))
+                #--------------------------------------------------------------
+        if epoch%save_model_step==0:
+            print('...Saving Checkpoint File in %s...'%(times_tamp))
+            saver.save(sess, saved_model,global_step=epoch)
+    tf.train.write_graph(sess.graph,saved_model_dir,'model.pbtxt')
 
 
 
